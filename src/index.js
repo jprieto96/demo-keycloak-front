848753957d8b5ff7036446cfc3e3201d5eef99cd
Jose Prieto <jose.delprieto@stratesys-ts.com>
2022-05-03 12:53:39 +0200

    reactDom.render removed of react 18

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from 'App'
 import { KeycloackContextProvider } from "KeycloakContext"
- 
-ReactDOM.render(
+
+const rootElement = document.getElementById('root')
+const root = ReactDOM.createRoot(rootElement)
+
+root.render(
     <KeycloackContextProvider>
         <App/>
-    </KeycloackContextProvider>,
-    document.getElementById("root")
-);
+    </KeycloackContextProvider>
+)
